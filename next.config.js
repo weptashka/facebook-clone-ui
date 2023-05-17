2c9f989f547b885f1b998c42f01fe298ff4b363b
Polly Laptop <p.logash@gmail.com>
2023-05-17 18:30:31 +0300

    feat: facebook app clone frontendp part

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains:["icons.iconarchive.com", "platform-lookaside.fbsbx.com", "images.pexels.com", "www.flaticon.com"],
+  },
+};
 
-module.exports = nextConfig
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import store from "../public/src/app/store";
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
+  );
 }
+
+export default MyApp;
